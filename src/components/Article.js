ead41fa0a66e0b41c935cc97eb13ef3669eb899a
Mohammad Shamchi <shamchimohammad@gmail.com>
2023-12-20 00:45:41 +0330

    After some edit in news api request front

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,23 +1,36 @@
+// src/components/Article.js or src/pages/Article.js
+
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Articles = () => {
     const [articles, setArticles] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState('');
 
-useEffect(() => {
-    axios.get('http://127.0.0.1:8000/api/articles')
-         .then(response => {
-             setArticles(response.data);
-         })
-         .catch(error => {
-             if (error.response && error.response.status === 429) {
-                 console.error('Too many requests. Please try again later.');
-             } else {
-                 console.error('There was an error fetching the articles', error);
-             }
-         });
-}, []);
+    useEffect(() => {
+        // Helper function to fetch articles
+        const fetchArticles = async () => {
+            setLoading(true);
+            try {
+                const response = await axios.get('http://127.0.0.1:8000/api/articles');
+                setArticles(response.data);
+            } catch (error) {
+                if (error.response && error.response.status === 429) {
+                    setError('Too many requests. Please try again later.');
+                } else {
+                    setError('An error occurred while fetching the articles.');
+                }
+            }
+            setLoading(false);
+        };
 
+        // Invoke the fetchArticles function
+        fetchArticles();
+    }, []); // The empty array ensures this effect only runs once
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error: {error}</p>;
 
     return (
         <div>
